84bfd28f6da7c5bf3f7d35e364455a6d57fc88de
Rex Stayer-Suprick <rstayers@student10.cse.nd.edu>
2024-02-26 11:03:01 -0500

    d

--- HW03/weather.js
+++ HW03/weather.js
@@ -1,13 +1,22 @@
-function toCelsius(){
+function toCelsius() {
+    // grabs the input from the user
+    let input =  document.getElementById("temperature").value;
+    
+    // Check if the input is a valid number
+    if (!isNaN(input)) {
+        // converts the temperature to Celsius
+        let celsius = (input - 32) * 5/9;
 
-	// grabs the input from the user
-	let input =  document.getElementById("temperature").value;
-	// converts the temperature to C
-	let celsius = (input - 32) * 5/9;
+        // show back to the user, on the <span> element
+        document.getElementById("result").innerText = celsius;
 
-	// show back to the user, on the <span> element
-	document.getElementById("result").innerText = celsius;
-	// makes the div visible
-	// element.style can be used to change CSS properties of an HTML document
-	document.getElementById("result-parent").style.visibility = "visible";
+        // makes the div visible
+        document.getElementById("result-parent").style.visibility = "visible";
+    } else {
+        // Display warning message for invalid input
+        document.getElementById("result").innerHTML = "<b style='color: red;'>Please input a valid number!</b>";
+        // make the div visible
+        document.getElementById("result-parent").style.visibility = "visible";
+    }
 }
+
